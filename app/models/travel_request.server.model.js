4002428f721acd47fc6262f4a3369bf77774bc31
Allyjin90 <ally.jin90@gmail.com>
2016-05-05 15:16:46 +0900

    first commit

---
+++ app/models/travel_request.server.model.js
@@ -0,0 +1,38 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Load the module dependencies
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+// Define a new 'ArticleSchema'
+var ArticleSchema = new Schema({
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	title: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'Title cannot be blank'
+	},
+	content: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	creator: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+// Create the 'Article' model out of the 'ArticleSchema'
+mongoose.model('Article', ArticleSchema);
+
+ travel_point:[String];
+ travel_concept:[String];
+ travel_start_day: Date;
+ travel_end_day: Date;
+ travel_plan_money: Number;
